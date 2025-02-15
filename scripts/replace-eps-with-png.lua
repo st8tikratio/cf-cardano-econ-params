c251584520f7c957a7663f42a5728a685c506f13
Fabian Bormann <fabian.bormann@cardanofoundation.org>
2025-02-15 22:16:30 +0100

    chore: adjust image width

--- scripts/replace-eps-with-png.lua
+++ scripts/replace-eps-with-png.lua
@@ -1,6 +1,6 @@
 function Image(elem)
   -- Set a maximum width for all images
-  elem.attributes["width"] = "200px"
+  elem.attributes["width"] = "400px"
   elem.src = elem.src:gsub("%.eps$", ".png")
   elem.src = elem.src:gsub("../figures/", "./figures/")
   return elem
